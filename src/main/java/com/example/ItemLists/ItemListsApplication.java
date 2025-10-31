9b89ca6d68805c6f0ab92df3950fc9471197cb55
homemade <agent-001panamka@mail.ru>
2025-10-31 18:13:03 +0500

    started

---
+++ src/main/java/com/example/ItemLists/ItemListsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ItemLists;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemListsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemListsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ItemLists/ItemListsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ItemLists;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemListsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
